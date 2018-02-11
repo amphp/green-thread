83dc57e80d48e49ade0c5224e0e3f078f87e1775
Aaron Piotrowski <aaron@trowski.com>
2018-02-11 12:37:31 -0600

    Add exception class

---
+++ src/InvalidAwaitError.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Amp\GreenThread;
+
+class InvalidAwaitError extends \Error {
+    /**
+     * @param mixed $awaited
+     * @param string $prefix
+     * @param \Throwable|null $previous
+     */
+    public function __construct($awaited, string $prefix, \Throwable $previous = null) {
+        parent::__construct($prefix . \sprintf(
+            "; %s awaited",
+            \is_object($awaited) ? \get_class($awaited) : \gettype($awaited)
+        ));
+    }
+}

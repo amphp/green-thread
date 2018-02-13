e13327a84be67d289aec87984f9d5c8e1fddd471
Aaron Piotrowski <aaron@trowski.com>
2018-02-12 22:10:12 -0600

    Update examples

--- examples/simultaneous-async.php
+++ examples/simultaneous-async.php
@@ -14,17 +14,23 @@ $callback = function (int $id): int {
 };
 
 Loop::run(coroutine(function () use ($callback): void {
+    $timer = Loop::repeat(100, function () {
+        echo ".", PHP_EOL; // This repeat timer is to show the loop is not being blocked.
+    });
+
     // Invoking $callback returns an int, but is executed asynchronously.
-    $result = $callback(1); // Call a subroutine within this green thread.
-    var_dump($result); // Executed after 1 second.
+    $result = $callback(1); // Call a subroutine within this green thread, taking 1 second to return.
+    var_dump($result);
 
     // Simultaneously runs two new green threads, await their resolution in this green thread.
-    $result = await([
+    $result = await([  // Executed simultaneously, only 1 second will elapse during this await.
         async($callback, 2),
-        async($callback, 3), // Executed simultaneously, only 1 second will elapse during this await.
+        async($callback, 3),
     ]);
     var_dump($result); // Executed after 2 seconds.
 
-    $result = $callback(4);
-    var_dump($result); // Executed after 3 seconds.
+    $result = $callback(4); // Call takes 1 second to return.
+    var_dump($result);
+
+    Loop::cancel($timer);
 }));

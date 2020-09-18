6bf6f6e8188bcc680f4715a183718176c3c51afd
Aaron Piotrowski <aaron@trowski.com>
2020-09-18 13:18:30 -0500

    Update examples

--- examples/nested-wrap.php
+++ examples/nested-wrap.php
@@ -5,7 +5,6 @@ require \dirname(__DIR__) . "/vendor/autoload.php";
 use Amp\Loop;
 use function Amp\asyncCallable;
 use function Amp\await;
-use function Amp\awaitPending;
 use function Amp\delay;
 
 Loop::defer(asyncCallable(function () {
@@ -15,5 +14,3 @@ Loop::defer(asyncCallable(function () {
 
 await(delay(10));
 print 'outer';
-
-awaitPending();
